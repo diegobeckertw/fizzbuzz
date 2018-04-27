b259ccd482662272e9e5a63cbbfce62fc4696d58
diegobeckertw <dbecker@thoughtworks.com>
2018-04-27 16:59:35 -0300

    Created method isMultipleByThree and test methods

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,14 +1,23 @@
 
-import org.hamcrest.MatcherAssert;
 import static org.hamcrest.core.Is.is;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
 
 public class FizzBuzzTest{
 
     @Test
-    public void isTrueWhenTheNumberIsMultipleByThree(){
+    public void shouldReturnTrueWhenTheNumberIsMultipleByThree(){
         FizzBuzz fizzbuzz = new FizzBuzz();
 
-        MatcherAssert.assertThat(true, is(fizzbuzz.isMultipleByThree(6)));        
+        assertThat("Number must be multiple of three!",fizzbuzz.isMultipleByThree(6), is(true)); 
+        // assertThat(reason, actual, matcher);       
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheNumberNotIsMultipleByThree(){
+        FizzBuzz fizzbuzz = new FizzBuzz();
+
+        assertThat(fizzbuzz.isMultipleByThree(4), is(false));     
     }
 }
