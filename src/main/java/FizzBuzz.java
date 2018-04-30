1dcf12d09ccd900130ad493bd990ef89199b296b
diegobeckertw <dbecker@thoughtworks.com>
2018-04-30 10:39:08 -0300

    Created main and final tests

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,14 +1,30 @@
 public class FizzBuzz{
 
-    public boolean isMultipleByThree(int num){
+    public static boolean isMultipleByThree(int num){
         return (num % 3 == 0);
     }
 
-    public boolean isMultipleByFive(int num){
+    public static boolean isMultipleByFive(int num){
         return (num % 5 == 0);
     }
 
-	public boolean isMultipleByThreeAndFive(int num) {
+	public static boolean isMultipleByThreeAndFive(int num) {
 		return (isMultipleByThree(num) && isMultipleByFive(num));
-	}
+    }
+    
+    public static void main(String[] args) {
+        String msgReturn;
+        for(int i = 1; i <= 100; i++){
+            if(isMultipleByThreeAndFive(i)){
+                msgReturn = "FizzBuzz";
+            }else if(isMultipleByFive(i)){
+                msgReturn = "Buzz";
+            }else if(isMultipleByThree(i)){
+                msgReturn = "Fizz";
+            }else{
+                msgReturn = Integer.toString(i);
+            }
+            System.out.println(msgReturn);
+        }
+    }
 }
