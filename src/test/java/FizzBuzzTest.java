05ca6d472b6870262765aa2f187374dad199dd83
diegobeckertw <dbecker@thoughtworks.com>
2018-05-02 09:55:03 -0300

    Created the init method to initialize the class before the tests

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -2,50 +2,46 @@
 import static org.hamcrest.core.Is.is;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest{
 
+    private FizzBuzz fizzbuzz;
+
+    @Before
+    public void init(){
+        this.fizzbuzz = new FizzBuzz();
+    }
+
     @Test
     public void shouldReturnTrueWhenTheNumberIsMultipleByThree(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
-        assertThat(fizzbuzz.isMultipleByThree(6), is(true)); 
-        // assertThat(reason, actual, matcher);       
+        assertThat(fizzbuzz.isMultipleByThree(6), is(true));      
     }
 
     @Test
     public void shouldReturnFalseWhenTheNumberNotIsMultipleByThree(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
         assertThat(fizzbuzz.isMultipleByThree(4), is(false));     
     }
 
     @Test
     public void shouldReturnTrueWhenTheNumberIsMultipleByFive(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
         assertThat(fizzbuzz.isMultipleByFive(10), is(true));
     }
 
     @Test
     public void shouldReturnFalseWhenTheNumberNotIsMultipleByFive(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
         assertThat(fizzbuzz.isMultipleByFive(8), is(false));
     }
 
     @Test
     public void shouldReturnTrueWhenTheNumberIsMultipleByThreeAndFive(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
         assertThat(fizzbuzz.isMultipleByThreeAndFive(15), is(true));
     }
 
     @Test
     public void shouldReturnFalseWhenTheNumberNotIsMultipleByThreeAndFive(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
-
         assertThat(fizzbuzz.isMultipleByThreeAndFive(8), is(false));
     }
 }
